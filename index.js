10fa9a8c74fa53d895fbd3a32ad5e6387766d3c8
admiralappsec <80486690+admiralappsec@users.noreply.github.com>
2021-06-23 22:00:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//import os module from npm
+//import modules from npm
 const os = require('os');
 const path = require('path');
 const download = require('mvn-artifact-download');
@@ -11,26 +11,26 @@ const getPlatform = () => {
     try {
         let platformBox = os.platform();
     
-        if (platformBox.includes('win32')) {
-            //console.log('this is a windows box');
+        if (platformBox.includes(`win32`)) {
+            //console.log(`this is a windows box`);
             return 'windows';
         }
-        if (platformBox.includes('linux')) {
-            //console.log('this is a linux box');
+        if (platformBox.includes(`linux`)) {
+            //console.log(`this is a linux box`);
             return 'linux';
         } 
-        if (platformBox.includes('darwin')) {
-            //console.log('this is a mac box');
+        if (platformBox.includes(`darwin`)) {
+            //console.log(`this is a mac box`);
             return 'mac';
         }
     } catch {
-        throw 'this os type is not currently supported.  possible options are: aix, darwin, freebsd, linux, openbsd, sunos, win32.'
+        throw `this os type is not currently supported.  possible options are: aix, darwin, freebsd, linux, openbsd, sunos, win32.`
     }
 }
 
 //download a Contrast Security agent into a directory
 const downloadContrastSecurity = ( platformType, agentType, customDownloadLocation, URL ) => {
-    let incomingCustomDownloadLocation = "";
+    const incomingCustomDownloadLocation = customDownloadLocation;
     const incomingPlatformType = platformType;
     const incomingAgentType = agentType;
     const incomingURL = URL;
@@ -39,66 +39,67 @@ const downloadContrastSecurity = ( platformType, agentType, customDownloadLocati
     //check if file already exists!
     try {
         if (customDownloadLocation != "") {
-            console.log('custom download location was detected.  moving forward with string ' + customDownloadLocation + '.');
+            console.log('custom download location was detected.  moving forward with string ' + incomingCustomDownloadLocation + '.');
 
             downloadLocation = customDownloadLocation;
         }
 
         if (customDownloadLocation == 'local') {
-            console.log('custom download located was detected as local.  Using current directory as download location.');
+            console.log(`custom download located was detected as local.  Using current directory as download location.`);
             downloadLocation = process.cwd();
+            console.log(`current working directory: downloadLocation`);
         }
 
         if (customDownloadLocation == "") {
-            console.log('no custom download location was passed to the function.  moving forward with default file locations per platform type.');
+            console.log(`no custom download location was passed to the function.  moving forward with default file locations per platform type.`);
 
-            if (incomingPlatformType == 'mac') {
+            if (incomingPlatformType == `mac`) {
                 downloadLocation = process.cwd();
             }
         
-            if (incomingPlatformType == 'windows') {
-                downloadLocation = path.join('/', 'C', 'Program Files', 'Contrast', incomingAgentType);
+            if (incomingPlatformType == `windows`) {
+                downloadLocation = path.join(`/`, `C`, `Program Files`, `Contrast`, incomingAgentType);
             }
-            if (incomingPlatformType == 'linux') {
-                downloadLocation = path.join('/', 'opt', 'contrast');
+            if (incomingPlatformType == `linux`) {
+                downloadLocation = path.join(`/`, `opt`, `contrast`);
             }
             
         }
         
         //check against incoming agent type == java
-        if (incomingAgentType == 'java') {
+        if (incomingAgentType == `java`) {
             //download the artifact using the npm library
             download.default({
-                groupId: 'com.contrastsecurity',
-                artifactId: 'contrast-agent',
+                groupId: `com.contrastsecurity`,
+                artifactId: `contrast-agent`,
                 //need to find the syntax to pass 'LATEST' version
-                version: '3.8.4.20143'
-            }, downloadLocation).then((results) => {console.log('file was downloaded to: ' + results)});
+                version: `3.8.4.20143`
+            }, downloadLocation).then((results) => {console.log(`file was downloaded to: ${results}`)});
 
         }
 
-        if (incomingAgentType == 'dotnet-legacy') {
-            console.log('coming soon.');
+        if (incomingAgentType == `dotnet-legacy`) {
+            console.log(`coming soon.`);
         }
 
-        if (incomingAgentType == 'dotnet-core') {
-            console.log('coming soon.');
+        if (incomingAgentType == `dotnet-core`) {
+            console.log(`coming soon.`);
         }
 
-        if (incomingAgentType == 'python') {
-            console.log('coming soon.');
+        if (incomingAgentType == `python`) {
+            console.log(`coming soon.`);
         }
 
-        if (incomingAgentType == 'golang') {
-            console.log('coming soon.');
+        if (incomingAgentType == `golang`) {
+            console.log(`coming soon.`);
         }
 
-        if (incomingAgentType == 'nodejs') {
-            console.log('coming soon.');
+        if (incomingAgentType == `nodejs`) {
+            console.log(`coming soon.`);
         }
 
-        if (incomingAgentType == 'ruby') {
-            console.log('coming soon.');
+        if (incomingAgentType == `ruby`) {
+            console.log(`coming soon.`);
         }
 
     } catch(error) {
@@ -109,12 +110,12 @@ const downloadContrastSecurity = ( platformType, agentType, customDownloadLocati
 
 //main try-catch statement for github actions
 try {
-    const agentType = core.getInput('agent-type');
+    const agentType = core.getInput(`agent-type`);
     console.log(`Agent type: ${agentType}`);
 
     const retrievedOSType = getPlatform();
 
-    downloadContrastSecurity(retrievedOSType, agentType, 'local', '' );
+    downloadContrastSecurity(retrievedOSType, agentType, `local`, '' );
 
     const successMessage = `successfully downloaded Contrast Security ${agentType} agent`;
     core.setOutput("success-message", successMessage);
